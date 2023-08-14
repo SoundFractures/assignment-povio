85bb7acf5b6b26dc37918766aaa17f62a04cb7d4
Sound Fractures <muziiix.sound@gmail.com>
2023-08-14 11:27:00 +0200

    feat: :package: Add and configure Next-Intl for translations

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,12 @@
+// import Image from 'next/image'
+// import styles from '~/styles/pages/Home.module.scss'
+import { useTranslations } from 'next-intl'
+
+export default function Home() {
+  const t = useTranslations('index')
+  return (
+    <main>
+      <h1 className="page-title">{t('title')}</h1>
+    </main>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path')
+
+const withNextIntl = require('next-intl/plugin')('./i18n.ts')
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
+
+module.exports = withNextIntl(nextConfig)
