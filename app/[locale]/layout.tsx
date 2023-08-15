0ef6164bea80d6f398cc5d9efa9a2b402ce15991
Sound Fractures <muziiix.sound@gmail.com>
2023-08-15 14:23:42 +0200

    feat: :sparkles: Wrap layout with locale and store providers

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import '~/styles/main.scss'
 import type { Metadata } from 'next'
-import TheNavigation from '~/components/Navigation'
+import { NextIntlClientProvider } from 'next-intl'
+import { notFound } from 'next/navigation'
+import Appbar from '~/components/appbar'
+import NavigationMobile from '~/components/navigation/Mobile'
+import StoreProvider from '~/components/StoreProvider'
 
 export const metadata: Metadata = {
   title: 'Povio - Assignment',
   description: 'Vetting assignment for Povio',
 }
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
-    <body>
-      <main>
-        <TheNavigation />
+export function generateStaticParams() {
+  return [{ locale: 'en' }]
+}
 
-        {children}
-      </main>
-    </body>
-  </html>
-)
+const RootLayout = async ({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode
+  params: {
+    locale: string
+  }
+}) => {
+  let messages
+  try {
+    messages = (await import(`~/locale/${locale}.json`)).default
+  } catch (error) {
+    notFound()
+  }
+  return (
+    <html lang="en">
+      <body>
+        <main>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <StoreProvider>
+              <Appbar />
+              <NavigationMobile />
+
+              {children}
+            </StoreProvider>
+          </NextIntlClientProvider>
+        </main>
+      </body>
+    </html>
+  )
+}
 
 export default RootLayout
