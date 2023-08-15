b2b6bf5436a3e59819351ffdb09b4d0dc85a21ad
Sound Fractures <muziiix.sound@gmail.com>
2023-08-15 11:20:18 +0200

    feat: :sparkles: Add store functionality to the NavigationMenuIcon

--- components/Navigation/MenuIcon.tsx
+++ components/Navigation/MenuIcon.tsx
@@ -1,15 +1,32 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 
-const MenuIcon = () => (
-  <button className="navigation-hamburger" type="button">
-    <Image
-      src="/assets/icons/hamburger.svg"
-      alt="Menu"
-      width={24}
-      height={16}
-    />
-  </button>
-)
+import useStore from '~/services/useStore'
+
+const MenuIcon = () => {
+  const { useStoreDispatch, useStoreSelector, actions } = useStore()
+
+  const dispatch = useStoreDispatch()
+  const isMobileNavOpen = useStoreSelector(
+    (state) => state.layout.isMobileNavOpen,
+  )
+  const handleSetMobileNavOpen = () => {
+    dispatch(actions.layoutSlice.setMobileNavOpen(!isMobileNavOpen))
+  }
+
+  return (
+    <button className="navigation-hamburger" type="button">
+      <Image
+        src={`/assets/icons/${isMobileNavOpen ? 'close' : 'hamburger'}.svg`}
+        alt="Menu"
+        width={24}
+        height={24}
+        onClick={handleSetMobileNavOpen}
+      />
+    </button>
+  )
+}
 
 export default MenuIcon
