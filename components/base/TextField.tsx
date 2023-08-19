b928483f3862943c183c3950c9520ecf98cfa29d
SoundFractures <soundfractures@gmail.com>
2023-08-19 18:36:27 +0200

    feat: :sparkles: Update Textfield with error functionality

--- components/base/TextField.tsx
+++ components/base/TextField.tsx
@@ -3,41 +3,50 @@
 import React from 'react'
 
 type Props = {
+  id: string
   label: string
   value: string
   onChange: React.Dispatch<React.SetStateAction<string>>
   onBlur?: React.Dispatch<React.SetStateAction<string>>
-  htmlFor: string
   className?: string
   type?: 'text' | 'password'
+  error?: string
 }
 
 const BaseTextField = ({
+  id,
   value,
   onChange,
   onBlur = () => {},
   label,
-  htmlFor,
   className = '',
   type = 'text',
+  error = '',
 }: Props) => {
-  const textFieldClassName = `base-text-field ${className}`
+  const isError = error.length > 0
+  const textFieldClassName = `base-text-field ${className} ${
+    isError ? 'base-error-text-field' : ''
+  }`
 
   return (
-    <div className={textFieldClassName}>
-      <div className="base-text-field-content">
-        <label htmlFor={htmlFor} className="base-text-field-label">
-          {label}
-        </label>
-        <input
-          id={htmlFor}
-          type={type}
-          value={value}
-          className="base-text-field-input"
-          onChange={(event) => onChange(event.target.value)}
-          onBlur={(event) => onBlur(event.target.value)}
-        />
+    <div>
+      <div className={textFieldClassName}>
+        <div className="base-text-field-content">
+          <label htmlFor={id} className="base-text-field-label">
+            {label}
+          </label>
+
+          <input
+            id={id}
+            type={type}
+            value={value}
+            className="base-text-field-input"
+            onChange={(event) => onChange(event.target.value)}
+            onBlur={(event) => onBlur(event.target.value)}
+          />
+        </div>
       </div>
+      {isError && <div className="base-text-field-error-caption">{error}</div>}
     </div>
   )
 }
