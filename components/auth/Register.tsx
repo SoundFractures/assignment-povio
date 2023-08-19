46e14f65f613dd91ccb85b03090245533935ba15
SoundFractures <soundfractures@gmail.com>
2023-08-19 18:53:35 +0200

    feat: :sparkles: Add validation to register

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -2,11 +2,13 @@
 
 import { useState } from 'react'
 import { useTranslations } from 'next-intl'
+import { useFormik } from 'formik'
 import useStore from '~/services/useStore'
 import Modal from '~/components/shared/Modal'
 import TextField from '~/components/base/TextField'
 import Button from '~/components/base/Button'
 import AuthCancel from '~/components/auth/AuthCancel'
+import { registerSchema } from '~/utils/validation'
 
 const AuthRegister = () => {
   const t = useTranslations('auth')
@@ -23,27 +25,22 @@ const AuthRegister = () => {
     dispatch(actions.layout.setRegisterModalOpen(false))
   }
   // Form state
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [dateOfBirth, setDateOfBirth] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const isFormValid = () =>
-    firstName.length > 0 &&
-    lastName.length > 0 &&
-    dateOfBirth.length > 0 &&
-    email.length > 0 &&
-    password.length > 0
-
-  // Register
+  // Form state and validation
   const [loading, setLoading] = useState(false)
-  const handleRegister = () => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 2000)
-  }
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      email: '',
+      password: '',
+    },
+    validationSchema: registerSchema(useTranslations('validation')),
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2))
+    },
+  })
+  // Left off: Touched not working
   return (
     <Modal
       isOpen={isRegisterModalOpen}
@@ -52,45 +49,51 @@ const AuthRegister = () => {
     >
       <div className="form-row">
         <TextField
-          value={firstName}
-          onChange={setFirstName}
+          value={formik.values.firstName}
+          onChange={(value) => formik.setFieldValue('firstName', value)}
           label={t('form.firstName')}
           id="firstName"
-          className="flex-grow"
+          error={!!formik.errors.firstName && formik.touched.firstName}
+          errorText={formik.errors.firstName}
         />
         <TextField
-          value={lastName}
-          onChange={setLastName}
+          value={formik.values.lastName}
+          onChange={(value) => formik.setFieldValue('lastName', value)}
           label={t('form.lastName')}
           id="lastName"
-          className="flex-grow"
+          error={!!formik.errors.lastName && formik.touched.lastName}
+          errorText={formik.errors.lastName}
         />
       </div>
       <TextField
-        value={dateOfBirth}
-        onChange={setDateOfBirth}
+        value={formik.values.dateOfBirth}
+        onChange={(value) => formik.setFieldValue('dateOfBirth', value)}
         label={t('form.dateOfBirth')}
         id="dateOfBirth"
+        error={!!formik.errors.dateOfBirth && formik.touched.dateOfBirth}
+        errorText={formik.errors.dateOfBirth}
       />
       <TextField
-        value={email}
-        onChange={setEmail}
+        value={formik.values.email}
+        onChange={(value) => formik.setFieldValue('email', value)}
         label={t('form.emailAddress')}
         id="emailAddress"
+        error={!!formik.errors.email && formik.touched.email}
+        errorText={formik.errors.email}
       />
       <TextField
-        value={password}
-        onChange={setPassword}
+        value={formik.values.password}
+        onChange={(value) => formik.setFieldValue('password', value)}
         label={t('form.password')}
         id="password"
         type="password"
+        error={!!formik.errors.password && formik.touched.password}
+        errorText={formik.errors.password}
       />
       <Button
         text={t('register.submit')}
         className="w-100 mt-5"
         loading={loading}
-        disabled={!isFormValid()}
-        onClick={handleRegister}
       />
 
       <AuthCancel onClick={handleSetRegisterModalClose}>
